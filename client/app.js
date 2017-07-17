52eb913a92fd87fb7224e178e0f88092e570e4a7
aviteller <aviteller2@gmail.com>
2017-07-17 10:11:51 +0100

    full working multiplayer tetris

---
+++ client/app.js
@@ -0,0 +1,49 @@
+
+const tetrisManager = new TetrisManager(document);
+const localTetris = tetrisManager.createPlayer();
+localTetris.element.classList.add('local');
+localTetris.run();
+
+const connectionManager = new ConnectionManager(tetrisManager);
+connectionManager.connect('ws://localhost:9000');
+
+const keyListener = (event) => {
+	[
+		[90, 67, 81, 87, 88],
+		[97, 99, 104, 105, 98],
+	].forEach((key, index) => {
+		const player = localTetris.player;
+		if(event.type === 'keydown') {
+			if (event.keyCode === key[0]) {
+					player.move(-1);
+			//move right
+				} else if (event.keyCode === key[1]) {
+					player.move(1);
+				//drop dwon extra postiotn
+				}  else if (event.keyCode === key[2]) {
+					player.rotate(-1)
+				} else if (event.keyCode === key[3]) {
+					player.rotate(1)
+				}
+			}
+			if (event.keyCode === key[4]) {
+				if (event.type === 'keydown') {
+					if (player.dropInterval !== player.DROP_FAST){						
+						player.drop();
+						player.dropInterval = player.DROP_FAST;
+					}
+				} else {
+					player.dropInterval = player.DROP_SLOW;
+				}
+			}
+		});
+};
+
+
+
+//moveive pieces based on keys
+document.addEventListener('keydown', keyListener);
+document.addEventListener('keyup', keyListener);
+
+
+
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tetris</title>
+	<style>
+		body{
+			background: #202028;
+			color: #fff;
+			display: flex;
+			font-family: sans-serif;
+			font-size: 2em;
+			text-align: center;
+		}
+		.player{
+			flex:1 1 auto;
+		}
+
+		.player.local canvas {
+			border-color: white;
+		}
+
+		canvas {
+			border: solid .2em #707070;
+			height:90vh;
+		}
+	</style>
+</head>
+<body>
+<template id="player-template">
+	<div class="player">
+		<div class="score"></div>
+		<canvas class="tetris" width="240" height="400"></canvas>
+	</div>
+</template>
+
+
+
+	<script src="client/tetris.js"></script>
+	<script src="client/tetrismanager.js"></script>
+	<script src="client/connection-manager.js"></script>
+	<script src="client/events.js"></script>
+	<script src="client/arena.js"></script>
+	<script src="client/player.js"></script>
+	<script src="client/app.js"></script>
+</body>
+</html>
